d42b2f24a35448429a5d892bfdaf0575f0be51c6
princek <princesamuelpks@gmail>
2024-02-03 10:26:28 +0000

    Update tips.dart

--- lib/screens/tabs/tips.dart
+++ lib/screens/tabs/tips.dart
@@ -1,4 +1,8 @@
+import 'dart:ffi';
+
+import 'package:drippsafe/db/hive/initial_data.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class TipScreen extends StatefulWidget {
   const TipScreen({super.key});
@@ -8,160 +12,9 @@ class TipScreen extends StatefulWidget {
 }
 
 class _TipScreenState extends State<TipScreen> {
-  final tips = [
-    {
-      "tip": "Be Prepared",
-      "description":
-          "Always keep a few sanitary pads or tampons in your backpack or purse."
-    },
-    {
-      "tip": "Learn About Menstrual Products",
-      "description":
-          "Explore different types of menstrual products and find what works best for you."
-    },
-    {
-      "tip": "Track Your Menstrual Cycle",
-      "description":
-          "Use a period tracker app or calendar to keep track of your menstrual cycle."
-    },
-    {
-      "tip": "Stay Hygienic",
-      "description":
-          "Change your sanitary products regularly to maintain good hygiene."
-    },
-    {
-      "tip": "Pain Relief",
-      "description":
-          "Consider taking over-the-counter pain relievers like ibuprofen for menstrual cramps."
-    },
-    {
-      "tip": "Stay Active",
-      "description":
-          "Exercise can help alleviate menstrual cramps and improve your overall mood."
-    },
-    {
-      "tip": "Stay Hydrated",
-      "description":
-          "Drink plenty of water to help with bloating and overall well-being."
-    },
-    {
-      "tip": "Healthy Diet",
-      "description":
-          "Consume a balanced diet with plenty of fruits, vegetables, and whole grains."
-    },
-    {
-      "tip": "Comfortable Clothing",
-      "description":
-          "Wear comfortable, loose-fitting clothes during your period."
-    },
-    {
-      "tip": "Communication",
-      "description":
-          "Talk to a trusted adult, friend, or family member about your experiences and concerns."
-    },
-    {
-      "tip": "Educate Yourself",
-      "description":
-          "Learn about the menstrual cycle, reproductive health, and menstrual hygiene."
-    },
-    {
-      "tip": "Know Signs of Abnormalities",
-      "description":
-          "Be aware of signs of abnormal menstrual cycles and consult a healthcare professional if concerned."
-    },
-    {
-      "tip": "Mindful Rest",
-      "description":
-          "Allow yourself some rest and relaxation during your period."
-    },
-    {
-      "tip": "Stay Positive",
-      "description":
-          "Don't let your period interfere with your self-esteem; it's a natural part of being a woman."
-    },
-    {
-      "tip": "Carry Extra Supplies",
-      "description":
-          "Always carry extra sanitary products in case of emergencies or to help a friend."
-    },
-    {
-      "tip": "Discreet Disposal",
-      "description": "Learn proper disposal methods for used sanitary products."
-    },
-    {
-      "tip": "Heating Pads",
-      "description":
-          "Consider using a heating pad for soothing relief from menstrual cramps."
-    },
-    {
-      "tip": "Understand PMS",
-      "description":
-          "Recognize premenstrual syndrome (PMS) symptoms and find strategies to manage them."
-    },
-    {
-      "tip": "Emotional Well-being",
-      "description":
-          "Pay attention to your emotional well-being and practice self-care during your period."
-    },
-    {
-      "tip": "Seek Guidance",
-      "description":
-          "If you have questions or concerns, seek guidance from a healthcare professional or school nurse."
-    },
-    {
-      "tip": "Be Patient with Your Body",
-      "description":
-          "Understand that your menstrual cycle may take time to regulate."
-    },
-    {
-      "tip": "Be Eco-Friendly",
-      "description":
-          "Consider environmentally friendly menstrual products, such as reusable cloth pads or menstrual cups."
-    },
-    {
-      "tip": "Plan Ahead for Outings",
-      "description":
-          "Wear comfortable clothing and bring extra supplies if you have plans during your period."
-    },
-    {
-      "tip": "Yoga and Stretching",
-      "description":
-          "Gentle yoga or stretching exercises can help alleviate tension and discomfort."
-    },
-    {
-      "tip": "Stay Informed About Reproductive Health",
-      "description":
-          "Learn about reproductive health, contraception, and safe practices."
-    },
-    {
-      "tip": "Keep a Personal Diary",
-      "description":
-          "Maintain a diary to track your emotions, physical symptoms, and menstrual cycle patterns."
-    },
-    {
-      "tip": "Know When to Seek Help",
-      "description":
-          "If you experience severe pain or concerning symptoms, consult a healthcare professional promptly."
-    },
-    {
-      "tip": "Connect with Peers",
-      "description":
-          "Talk to friends who have already experienced menstruation for additional support and advice."
-    },
-    {
-      "tip": "Celebrate Milestones",
-      "description":
-          "Celebrate each milestone in your menstrual journey as a positive step toward adulthood."
-    },
-    {
-      "tip": "Be Kind to Yourself",
-      "description":
-          "Menstruation is a natural part of life; be kind to yourself and take things at your own pace."
-    }
-  ];
-
+  List tips = [];
   // display tips detail alert dialog
-  void _showTipsDetailDialog(tip, description, tipNo) {
+  void _showTipsDetailDialog(tip, description, tipNo, fav) {
     showDialog(
       barrierDismissible: false,
       context: context,
@@ -192,10 +45,72 @@ class _TipScreenState extends State<TipScreen> {
               },
               child: const Text('Close'),
             ),
+            IconButton(
+                onPressed: () {
+                  addFavTip(tipNo);
+                },
+                icon: Icon(
+                  Icons.favorite,
+                  color: fav ? Colors.red : Colors.grey,
+                )),
           ],
         );
       },
     );
+  }
+
+  void addFavTip(id) {
+    // change the favourite status of the tip in the box to true
+    var mybox = Hive.box('drippsafe_db');
+    var tipsData = mybox.get('tips').toList();
+    var tipIndex = tipsData.indexWhere((tip) => tip['id'] == id);
+    tipsData[tipIndex]['favourite'] = true;
+    mybox.put('tips', tipsData);
+    initializeTips();
+
+    // show a snackbar
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: const Text('Tip added to favourites!'),
+        duration: const Duration(seconds: 2),
+        behavior: SnackBarBehavior.floating,
+        width: MediaQuery.of(context).size.width * 0.8,
+      ),
+    );
+    Navigator.of(context).pop();
+  }
+
+  void initializeTips() async {
+    var mybox = Hive.box('drippsafe_db');
+    // construct list of tips as tip model
+    List tipsModelData = [];
+
+    if (mybox.get('tips') == null || mybox.get('tips').isEmpty) {
+      initialTips.forEach((tip) {
+        tipsModelData.add({
+          "id": (initialTips.indexOf(tip) + 1).toString(),
+          "title": tip['tip'],
+          "description": tip['description'],
+          "created_at": DateTime.now().toString(),
+          "favourite": false,
+        });
+      });
+      mybox.put('tips', tipsModelData);
+    }
+
+    // get the tips data from the box
+    List tipsData = mybox.get('tips').toList();
+
+    print(tipsData);
+    setState(() {
+      tips = tipsData;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializeTips();
   }
 
   @override
@@ -221,9 +136,10 @@ class _TipScreenState extends State<TipScreen> {
                   itemBuilder: (context, index) {
                     return InkWell(
                       onTap: () => _showTipsDetailDialog(
-                        tips[index]["tip"],
-                        tips[index]["description"],
-                        index + 1,
+                        tips[index]['title'],
+                        tips[index]['description'],
+                        tips[index]['id'],
+                        tips[index]['favourite'],
                       ),
                       child: Card(
                         elevation: 2,
@@ -250,7 +166,7 @@ class _TipScreenState extends State<TipScreen> {
                                             fontWeight: FontWeight.bold),
                                       ),
                                       Text(
-                                        tips[index]["tip"]!,
+                                        tips[index]['title'],
                                         style: const TextStyle(
                                           fontSize: 16,
                                         ),
@@ -272,7 +188,12 @@ class _TipScreenState extends State<TipScreen> {
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            // remove the tips from the box
+            var mybox = Hive.box('drippsafe_db');
+            mybox.delete('tips');
+            initializeTips();
+          },
           backgroundColor: Colors.pink[900],
           foregroundColor: Colors.white,
           child: const Icon(Icons.favorite),
