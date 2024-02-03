11127677a8156103f150d10452d9732257b17dac
princek <princesamuelpks@gmail>
2024-02-03 10:26:17 +0000

    Update tipmodel.dart

--- lib/db/models/tipmodel.dart
+++ lib/db/models/tipmodel.dart
@@ -1,27 +1,45 @@
-class TipModel {
-  int id;
-  String title;
-  String description;
-  DateTime createdAt;
+// import 'package:hive/hive.dart';
 
-  TipModel({
-    required this.id,
-    required this.title,
-    required this.description,
-    required this.createdAt,
-  });
+// part 'tipmodel.g.dart';
 
-  factory TipModel.fromJson(Map<String, dynamic> json) => TipModel(
-        id: json["id"],
-        title: json["title"],
-        description: json["description"],
-        createdAt: json["created_at"],
-      );
+// @HiveType(typeId: 0)
+// class TipModel {
+//   @HiveField(0)
+//   int id;
+//   @HiveField(1)
+//   String title;
+//   @HiveField(2)
+//   String description;
+//   @HiveField(3)
+//   DateTime createdAt;
+//   bool favourite;
 
-  Map<String, dynamic> toJson() => {
-        "id": id,
-        "title": title,
-        "description": description,
-        "created_at": createdAt,
-      };
-}
+//   TipModel({
+//     required this.id,
+//     required this.title,
+//     required this.description,
+//     required this.createdAt,
+//     this.favourite = false,
+//   });
+
+//   // gettters
+//   int get getId => id;
+//   String get getTitle => title;
+//   String get getDescription => description;
+//   DateTime get getCreatedAt => createdAt;
+//   bool get getFavourite => favourite;
+
+//   factory TipModel.fromJson(Map<String, dynamic> json) => TipModel(
+//         id: json["id"],
+//         title: json["title"],
+//         description: json["description"],
+//         createdAt: json["created_at"],
+//       );
+
+//   Map<String, dynamic> toJson() => {
+//         "id": id,
+//         "title": title,
+//         "description": description,
+//         "created_at": createdAt,
+//       };
+// }
